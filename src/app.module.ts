ed2b8d798249407a02128646c3254a4e3a7cd3ca
Gaetan Wcz <gaetan.wichlacz@ext.adeo.com>
2025-04-07 14:58:32 +0200

    feat(): add basic stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './modules/app/app.controller';
+import { AppService } from './modules/app/app.service';
+import { ConfigModule } from '@nestjs/config';
+import { configuration } from 'src/config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true, // config accessible globalement
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService
+  ]
 })
 export class AppModule {}
