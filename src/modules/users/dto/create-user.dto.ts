b80b815e7fc693df81f6a4452be3019bb894ce5f
Gaetan Wcz <gaetan.wichlacz@ext.adeo.com>
2025-04-14 14:35:12 +0200

    feat(): add class validation

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,27 +1,32 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty({
     description: 'First name',
     example: 'John',
   })
+  @IsString()
   firstName: string;
 
   @ApiProperty({
     description: 'Last name',
     example: 'Doe',
   })
+  @IsString()
   lastName: string;
 
   @ApiProperty({
     description: "User's password - for test only obviously",
     example: 'My4w3sOmEp4s5w0rD',
   })
+  @IsString()
   password: string;
 
   @ApiProperty({
     description: 'Email',
     example: 'johndoe@example.com',
   })
+  @IsEmail()
   email: string;
 }
